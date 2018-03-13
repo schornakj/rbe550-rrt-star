a43b7f101bd2281b9eda9db8081d81d4ca49c9be
Joe Schornak <jgschornak@wpi.edu>
2018-03-13 00:10:24 -0400

    Use KD trees

--- rrt.py
+++ rrt.py
@@ -9,7 +9,6 @@ import kdtree
 class Node:
     parent = None
     cost = 0
-
     def __init__(self, x, y, parent):
         self.coords = (x, y)
         self.parent = parent
@@ -32,17 +31,17 @@ class Node:
 
 
 class RRT:
-    nodes_start_tree = []
-    nodes_goal_tree = []
+    nodes_start_tree = kdtree.create(dimensions=2)
+    nodes_goal_tree = kdtree.create(dimensions=2)
 
     def __init__(self, path_to_map, use_rrt, coords_start, coords_end, step_size):
         self.image = Image.open(path_to_map)
         self.map = scipy.ndimage.imread(path_to_map, flatten=True)
         self.root_start = Node(coords_start[0], coords_start[1], None)
-        self.nodes_start_tree.append(self.root_start)
+        self.nodes_start_tree.add(self.root_start)
 
         self.root_goal = Node(coords_end[0], coords_end[1], None)
-        self.nodes_goal_tree.append(self.root_goal)
+        self.nodes_goal_tree.add(self.root_goal)
 
         self.radius = 50
 
@@ -90,16 +89,15 @@ class RRT:
         if self.check_if_obstacle_free(current_node.coords, (new_position_x, new_position_y)) and self.node_count < self.num_nodes:
             closer_node = Node(new_position_x, new_position_y, None)
 
-            # print("Closer node: " + str((closer_node.x, closer_node.y)))
             parent_new, cost_new = self.choose_parent(nodes, current_node, closer_node)
             closer_node.parent = parent_new
             closer_node.cost = cost_new
-            # closer_node.set_parent(current_node)
-            # closer_node.cost = closer_node.parent.cost + self.dist_euclidean((closer_node.x, closer_node.y), (current_node.x, current_node.y))
 
-            nodes.append(closer_node)
+            nodes.add(closer_node)
             self.node_count += 1
             self.rewire(nodes, closer_node)
+            if not nodes.is_balanced and self.node_count % 20 == 0:
+                nodes.rebalance()
         return closer_node
 
     def do_connect(self, nodes, origin_node, target_node):
@@ -107,7 +105,7 @@ class RRT:
         distance_to_target = self.dist_euclidean((current_node[0], current_node[1]), (target_node[0], target_node[1]))
         distance_from_origin = distance_to_target
         while distance_to_target > self.step_size and self.node_count < self.num_nodes:
-            print distance_to_target
+            # print distance_to_target
             new_position_x = current_node[0] + self.step_size*(target_node[0] - origin_node[1])/distance_from_origin
             new_position_y = current_node[1] + self.step_size*(target_node[1] - origin_node[1])/distance_from_origin
 
@@ -118,12 +116,14 @@ class RRT:
                 closer_node.parent = parent_new
                 closer_node.cost = cost_new
 
-                nodes.append(closer_node)
+                nodes.add(closer_node)
                 self.node_count += 1
 
                 current_node = closer_node
+                if not nodes.is_balanced and self.node_count % 20 == 0:
+                    nodes.rebalance()
             else:
-                print "Hit something, unfortunately"
+                # print "Hit something, unfortunately"
                 return False, None, None
             distance_to_target = self.dist_euclidean((current_node[0], current_node[1]), (target_node[0], target_node[1]))
         if distance_to_target <= self.step_size:
@@ -136,23 +136,26 @@ class RRT:
     def steer(self, x, y):
         return NotImplemented
 
-    def get_nearest_node(self, nodes, point):
-        distance_min = 10000
-        nearest = None
-        for node in nodes:
-            distance = self.dist_euclidean((node[0], node[1]), point)
-            if distance < distance_min:
-                nearest = node
-                distance_min = distance
-        return nearest
+    def get_nearest_node(self, nodes, node_origin):
+        return nodes.search_nn(node_origin)[0].data
+        # distance_min = 10000
+        # nearest = None
+        # for node in nodes:
+        #     distance = self.dist_euclidean((node[0], node[1]), point)
+        #     if distance < distance_min:
+        #         nearest = node
+        #         distance_min = distance
+        # return nearest
 
     def get_near_nodes(self, nodes, point):
-        distance = 2
-        near_nodes = []
-        for node in nodes:
-            if self.dist_euclidean(point, (node[0], node[1])) < distance:
-                near_nodes.append(node)
-        return near_nodes
+        return nodes.search_knn(point, 10)
+
+        # distance = 2
+        # near_nodes = []
+        # for node in nodes:
+        #     if self.dist_euclidean(point, (node[0], node[1])) < distance:
+        #         near_nodes.append(node)
+        # return near_nodes
 
     def check_if_obstacle_free(self, point_a, point_b):
         x_min = point_a[0]
@@ -170,18 +173,21 @@ class RRT:
             return False
 
     def rewire(self, nodes, new_node):
-        near_nodes = self.get_near_nodes(nodes, (new_node[0], new_node[1]))
-        for node in near_nodes:
-            if self.check_if_obstacle_free((node[0], node[1]), (new_node[0], new_node[1]))\
+        near_nodes = self.get_near_nodes(nodes, new_node.coords)
+        for kdnode in near_nodes:
+            node = kdnode[0].data
+            if self.check_if_obstacle_free(node.coords, new_node.coords)\
                     and new_node.parent is not node \
-                    and new_node.cost + self.dist_euclidean((node[0], node[1]), (new_node[0], new_node[1])) < node.cost:
+                    and new_node.cost + self.dist_euclidean(node.coords, new_node.coords) < node.cost:
                 node.parent = new_node
-                node.cost = new_node.cost + self.dist_euclidean(node.coords, (new_node[0], new_node[1]))
+                node.cost = new_node.cost + self.dist_euclidean(node.coords, new_node.coords)
 
     def choose_parent(self, nodes, nearest_node, new_node):
         nn = nearest_node
-        for node in nodes:
-            # if this node is within the radius of the new nodw
+        near_nodes = self.get_near_nodes(nodes, new_node)
+        for kdnode in near_nodes:
+            node = kdnode[0].data
+            # if this node is within the radius of the new node
             # and if the node cost plus distance to the new node is less than for the previous nearest node
             # make it the nearest node
             if self.dist_euclidean(node.coords, new_node.coords) < self.radius \
@@ -205,7 +211,8 @@ class RRT:
             draw.line([(0, row * scale_factor),
                        (self.map.shape[0] * scale_factor, row * scale_factor)], (0, 0, 0))
 
-        for node in self.nodes_start_tree:
+        for kdnode in kdtree.level_order(self.nodes_start_tree):
+            node = kdnode.data
             if node.parent is not None:
                 draw.line([(node[0] * scale_factor, node[1] * scale_factor), (node.parent[0] * scale_factor, node.parent[1] * scale_factor)], (255,0,255))
                 draw.ellipse([(node[0] * scale_factor - 2, node[1] * scale_factor - 2),
@@ -214,7 +221,8 @@ class RRT:
                 draw.ellipse([(node[0] * scale_factor - 2, node[1] * scale_factor - 2),
                               (node[0] * scale_factor + 2, node[1] * scale_factor + 2)], fill=(0,255,0), outline=(0,255,0))
 
-        for node in self.nodes_goal_tree:
+        for kdnode in kdtree.level_order(self.nodes_goal_tree):
+            node = kdnode.data
             if node.parent is not None:
                 draw.line([(node[0] * scale_factor, node[1] * scale_factor), (node.parent[0] * scale_factor, node.parent[1] * scale_factor)], (0,0,255))
                 draw.ellipse([(node[0] * scale_factor - 2, node[1] * scale_factor - 2),
@@ -255,7 +263,7 @@ class RRT:
 
             # point_random = self.sample()
             if new_node is not None:
-                nearest = self.get_nearest_node(self.nodes_goal_tree, new_node.coords)
+                nearest = self.get_nearest_node(self.nodes_goal_tree, new_node)
                 trees_linked, joint_node_goal, joint_node_start = self.do_connect(self.nodes_goal_tree, nearest, new_node)
                 if trees_linked:
                     path_start_tree = self.trace_path(joint_node_start)
